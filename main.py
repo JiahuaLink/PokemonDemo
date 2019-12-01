8df6f971886515fcc0d4c2922213fbd9d8ce2a12
840132699 <8400132699@qq.com>
2019-12-02 02:02:11 +0800

    html显示

--- main.py
+++ main.py
@@ -38,11 +38,36 @@ def index():
     defend_sp_value = pokemon["defend_sp_value"]
     speed_value = pokemon["speed_value"]
 
+    moves = pokemon["moves"]
+    moveinfo1 = moves["move1"]
+    moveinfo2 = moves["move2"]
+    moveinfo3 = moves["move3"]
+    moveinfo4 = moves["move4"]
+    
+    movename1=moveinfo1["name"]
+    movename2=moveinfo2["name"]
+    movename3=moveinfo3["name"]
+    movename4=moveinfo4["name"]
+    
+    movetype1=moveinfo1["type"]
+    movetype2=moveinfo2["type"]
+    movetype3=moveinfo3["type"]
+    movetype4=moveinfo4["type"]
+    
+    
+
+    
+    
+    
+    
+
+
+
     gifname = '%s.gif' % serial_num
     # moves=pokemon["moves"]
     # move1=moves["move1"]
 
-    return render_template('index.html', filename=gifname, serial_num=serial_num, pkmon_name=pkmon_name,level=level, pkmon_type1=pkmon_type1, pkmon_type2=pkmon_type2, ability=ability, hp_value=hp_value, atk_value=atk_value, defend_value=defend_value, atk_sp_value=atk_sp_value, defend_sp_value=defend_sp_value, speed_value=speed_value)
+    return render_template('index.html', filename=gifname, serial_num=serial_num, pkmon_name=pkmon_name,level=level, pkmon_type1=pkmon_type1, pkmon_type2=pkmon_type2, ability=ability, hp_value=hp_value, atk_value=atk_value, defend_value=defend_value, atk_sp_value=atk_sp_value, defend_sp_value=defend_sp_value, speed_value=speed_value,movename1=movename1,movename2=movename2,movename3=movename3,movename4=movename4,movetype1=movetype1,movetype2=movetype2,movetype3=movetype3,movetype4=movetype4)
 
 
 if __name__ == "__main__":
